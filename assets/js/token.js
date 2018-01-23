a1168fcd322cd83de2a9e625f13390befc40ba8f
Kill Yourself Coin <team@killyourself.network>
2018-01-22 20:30:54 -0600

    Got token JS all fixed up

--- assets/js/token.js
+++ assets/js/token.js
@@ -1,33 +1,395 @@
 var Web3 = require("web3");
-var web3 = new Web3(new Web3.providers.HttpProvider());
+var web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"));
 
 function getTokenStats(price) {
-    // TODO fill in actual abi code
-    var abi = [];
+    var abi = [{
+            "constant": true,
+            "inputs": [],
+            "name": "name",
+            "outputs": [{
+                "name": "",
+                "type": "string"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{
+                    "name": "_spender",
+                    "type": "address"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "approve",
+            "outputs": [{
+                "name": "success",
+                "type": "bool"
+            }],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "reservedTokens",
+            "outputs": [{
+                "name": "",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "totalSupply",
+            "outputs": [{
+                "name": "",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{
+                    "name": "_from",
+                    "type": "address"
+                },
+                {
+                    "name": "_to",
+                    "type": "address"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transferFrom",
+            "outputs": [{
+                "name": "success",
+                "type": "bool"
+            }],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "decimals",
+            "outputs": [{
+                "name": "",
+                "type": "uint8"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "version",
+            "outputs": [{
+                "name": "",
+                "type": "string"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "disablePurchasing",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "unitsOneEthCanBuy",
+            "outputs": [{
+                "name": "",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [{
+                "name": "_owner",
+                "type": "address"
+            }],
+            "name": "balanceOf",
+            "outputs": [{
+                "name": "balance",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "tokensIssued",
+            "outputs": [{
+                "name": "",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "availableSupply",
+            "outputs": [{
+                "name": "",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "owner",
+            "outputs": [{
+                "name": "",
+                "type": "address"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "enablePurchasing",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "totalEthInWei",
+            "outputs": [{
+                "name": "",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "symbol",
+            "outputs": [{
+                "name": "",
+                "type": "string"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{
+                    "name": "_to",
+                    "type": "address"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "transfer",
+            "outputs": [{
+                "name": "success",
+                "type": "bool"
+            }],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{
+                    "name": "_spender",
+                    "type": "address"
+                },
+                {
+                    "name": "_value",
+                    "type": "uint256"
+                },
+                {
+                    "name": "_extraData",
+                    "type": "bytes"
+                }
+            ],
+            "name": "approveAndCall",
+            "outputs": [{
+                "name": "success",
+                "type": "bool"
+            }],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [],
+            "name": "purchasingAllowed",
+            "outputs": [{
+                "name": "",
+                "type": "bool"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [{
+                    "name": "_owner",
+                    "type": "address"
+                },
+                {
+                    "name": "_spender",
+                    "type": "address"
+                }
+            ],
+            "name": "allowance",
+            "outputs": [{
+                "name": "remaining",
+                "type": "uint256"
+            }],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [{
+                "name": "_tokenContract",
+                "type": "address"
+            }],
+            "name": "withdrawForeignTokens",
+            "outputs": [{
+                "name": "",
+                "type": "bool"
+            }],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "inputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "constructor"
+        },
+        {
+            "payable": true,
+            "stateMutability": "payable",
+            "type": "fallback"
+        },
+        {
+            "anonymous": false,
+            "inputs": [{
+                    "indexed": true,
+                    "name": "_from",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "name": "_to",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Transfer",
+            "type": "event"
+        },
+        {
+            "anonymous": false,
+            "inputs": [{
+                    "indexed": true,
+                    "name": "_owner",
+                    "type": "address"
+                },
+                {
+                    "indexed": true,
+                    "name": "_spender",
+                    "type": "address"
+                },
+                {
+                    "indexed": false,
+                    "name": "_value",
+                    "type": "uint256"
+                }
+            ],
+            "name": "Approval",
+            "type": "event"
+        }
+    ];
 
     // creation of contract object
     var contract = web3.eth.contract(abi);
-    // TODO fill in actual address
-    var contractInstance = contract.at("");
-
+    var contractInstance = contract.at("0xc584a60e2cbedfe6a068371e6e34f05844b3111f");
 
     contractInstance.totalEthInWei(function (err, data) {
         // get eth raised
         ETHAmountRaised = data / 1000000000000000000;
+
         document.getElementById("ETHAmountRaised").innerHTML = ETHAmountRaised.toLocaleString();
         // calculate USD value
         document.getElementById("USDAmountRaised").innerHTML = (ETHAmountRaised * price).toFixed(2).toLocaleString();
 
-        contractInstance.tokensIssued(function (err, data) {
-            // get tokens issued
-            tokensIssued = data / 1000000000000000000;
-            document.getElementById("tokensIssued").innerHTML = tokensIssued.toLocaleString();
-            // calculate percentage of tokens sold
-            soldPercentage = (tokensIssued / availableSupply * 100).toFixed(2);
-            document.getElementById("soldPercentage").innerHTML = soldPercentage;
-            // set progress bar
-            document.getElementById("progress-interior").style.width = soldPercentage.toString() + "%";
-        });
+    });
+
+    contractInstance.tokensIssued(function (err, data) {
+        // get tokens issued
+        tokensIssued = data / 1000000000000000000;
+        document.getElementById("tokensIssued").innerHTML = tokensIssued.toLocaleString();
+        // calculate percentage of tokens sold
+        soldPercentage = (tokensIssued / availableSupply * 100).toFixed(2);
+        document.getElementById("soldPercentage").innerHTML = soldPercentage;
+        // set progress bar
+        document.getElementById("progress-interior").style.width = soldPercentage.toString() + "%";
+    });
+
+    contractInstance.purchasingAllowed(function (err, data) {
+        // get purchaseablity
+        if (data) {
+            // change color and text
+            document.getElementById("sale-notice").innerHTML = "The ICO is live!";
+            document.getElementById("sale-notice").style.backgroundColor = "rgb(91, 182, 121)";
+            document.getElementById("sale-notice").style.borderColor = "rgb(52, 139, 81)";
+        }
     });
 }
 
@@ -54,3 +416,5 @@ $(function () {
         $(".statsError").hide();
     } catch (err) {}
 });
+
+// JavaScript was a mistake
